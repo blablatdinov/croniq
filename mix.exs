2664c30dcce3b89337985fb31d488633246676e8
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-06-03 16:06:58 +0300

    lock deps

--- mix.exs
+++ mix.exs
@@ -32,17 +32,17 @@ defmodule Croniq.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.21"},
-      {:phoenix_ecto, "~> 4.5"},
-      {:ecto_sql, "~> 3.10"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.0"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
+      {:phoenix, "== 1.7.21"},
+      {:phoenix_ecto, "== 4.6.4"},
+      {:ecto_sql, "== 3.12.1"},
+      {:postgrex, "== 0.20.0"},
+      {:phoenix_html, "== 4.2.1"},
+      {:phoenix_live_reload, "== 1.6.0", only: :dev},
+      {:phoenix_live_view, "== 1.0.13"},
+      {:floki, "== 0.37.1", only: :test},
+      {:phoenix_live_dashboard, "== 0.8.7"},
+      {:esbuild, "== 0.10.0", runtime: Mix.env() == :dev},
+      {:tailwind, "== 0.2.4", runtime: Mix.env() == :dev},
       {:heroicons,
        github: "tailwindlabs/heroicons",
        tag: "v2.1.1",
@@ -50,14 +50,14 @@ defmodule Croniq.MixProject do
        app: false,
        compile: false,
        depth: 1},
-      {:swoosh, "~> 1.5"},
-      {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 1.0"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26"},
-      {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:swoosh, "== 1.19.1"},
+      {:finch, "== 0.19.0"},
+      {:telemetry_metrics, "== 1.1.0"},
+      {:telemetry_poller, "== 1.2.0"},
+      {:gettext, "== 0.26.2"},
+      {:jason, "== 1.4.4"},
+      {:dns_cluster, "== 0.1.3"},
+      {:bandit, "== 1.7.0"}
     ]
   end
 
