9004c531dfcc6016381e0874d1a44e6e531aea12
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2025-06-06 14:58:46 +0300

    Send requests implementation (#13)

--- lib/croniq/requests.ex
+++ lib/croniq/requests.ex
@@ -1,4 +1,17 @@
 defmodule Croniq.Requests do
+  import Ecto.Query
+  import HTTPoison
+  alias Croniq.Repo
+  alias Croniq.Task
+
   def send_request(task_id) do
+    task = Repo.get_by!(Task, id: task_id)
+
+    HTTPoison.request!(%HTTPoison.Request{
+      method: task.method,
+      url: task.url,
+      body: task.body,
+      headers: task.headers |> Enum.map(fn {key, value} -> {to_string(key), to_string(value)} end)
+    })
   end
 end
