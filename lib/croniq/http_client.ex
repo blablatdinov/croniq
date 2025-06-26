9ea39039205f444c73fc4ae3e9bf2f13564ef3f2
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-06-26 19:55:46 +0300

    lint

--- lib/croniq/http_client.ex
+++ lib/croniq/http_client.ex
@@ -1,3 +1,8 @@
 defmodule Croniq.HttpClient do
+  @moduledoc """
+  Behaviour for HTTP client implementations.
+
+  Defines the interface for making HTTP requests in the application.
+  """
   @callback request(HTTPoison.Request.t()) :: {:ok, HTTPoison.Response.t()} | {:error, any()}
 end
--- lib/croniq/http_client_httpoison.ex
+++ lib/croniq/http_client_httpoison.ex
@@ -1,4 +1,9 @@
 defmodule Croniq.HttpClient.HTTPoison do
+  @moduledoc """
+  HTTPoison implementation of the HttpClient behaviour.
+
+  Wraps HTTPoison library to provide HTTP request functionality.
+  """
   @behaviour Croniq.HttpClient
 
   @impl true
