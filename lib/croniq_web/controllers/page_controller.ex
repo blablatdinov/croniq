59eda2df78c94fd6c31292b4e0abf358068b329b
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2025-06-27 19:54:34 +0300

    documentation (#44)

--- lib/croniq_web/controllers/page_controller.ex
+++ lib/croniq_web/controllers/page_controller.ex
@@ -6,6 +6,37 @@ defmodule CroniqWeb.PageController do
   end
 
   def docs(conn, _params) do
-    render(conn, :docs)
+    auth_json =
+      Jason.encode!(%{"email" => "your@email.com", "password" => "yourpassword"}, pretty: true)
+
+    create_task_json =
+      Jason.encode!(
+        %{
+          "name" => "My API Call",
+          "schedule" => "* * * * *",
+          "url" => "https://api.example.com/endpoint",
+          "method" => "POST",
+          "headers" => %{"Content-Type" => "application/json"},
+          "body" => "{\"foo\":\"bar\"}"
+        },
+        pretty: true
+      )
+
+    update_task_json =
+      Jason.encode!(
+        %{
+          "name" => "Updated Name",
+          "schedule" => "*/5 * * * *",
+          "url" => "https://api.example.com/endpoint",
+          "method" => "GET"
+        },
+        pretty: true
+      )
+
+    render(conn, :docs,
+      auth_json: auth_json,
+      create_task_json: create_task_json,
+      update_task_json: update_task_json
+    )
   end
 end
