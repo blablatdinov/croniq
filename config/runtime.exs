2400f83d057808296b0e782d28cd7e108c9d82c2
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-07-03 20:28:23 +0300

    fix recaptcha

--- config/runtime.exs
+++ config/runtime.exs
@@ -86,4 +86,11 @@ if config_env() == :prod do
     api_key: brevo_api_key
 
   config :swoosh, :api_client, Swoosh.ApiClient.Finch
+
+  config :croniq, :recaptcha,
+    site_key: System.get_env("RECAPTCHA_SITE_KEY"),
+    secret_key: System.get_env("RECAPTCHA_SECRET_KEY"),
+    site_key_v2: System.get_env("RECAPTCHA_SITE_KEY_V2"),
+    secret_key_v2: System.get_env("RECAPTCHA_SECRET_KEY_V2"),
+    verify_url: "https://www.google.com/recaptcha/api/siteverify"
 end
