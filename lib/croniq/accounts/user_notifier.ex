530ff69ad37fe885ed8b1a34a96d73ae8c3e745f
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-07-04 15:05:27 +0300

    fix domain

--- lib/croniq/accounts/user_notifier.ex
+++ lib/croniq/accounts/user_notifier.ex
@@ -20,7 +20,7 @@ defmodule Croniq.Accounts.UserNotifier do
     email =
       new()
       |> to(recipient)
-      |> from({"Croniq", "dev@croniq.ilaletdionv.ru"})
+      |> from({"Croniq", "dev@croniq.ilaletdinov.ru"})
       |> subject(subject)
       |> text_body(body)
 
