41efe6e6d3a73b2e0874e985fb202d9b26d83010
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-07-06 00:45:50 +0300

    lint

--- lib/croniq/limit_notification.ex
+++ lib/croniq/limit_notification.ex
@@ -1,4 +1,9 @@
 defmodule Croniq.LimitNotification do
+  @moduledoc """
+  In-memory (ETS) storage for tracking when a user was last notified about exceeding the request limit.
+  Used to prevent sending multiple emails per day to the same user.
+  Data is lost on application restart.
+  """
   @table :limit_notification_sent
 
   def start_link do
